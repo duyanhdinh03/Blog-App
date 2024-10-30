ca6e7af5d909c6825e0dac0c2830ec48c9b70e3a
dduya171 <duyanhk94lc@gmail.com>
2024-10-30 23:46:26 +0700

    validation for Dto

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -5,6 +5,7 @@ import com.springboot.blog.payload.Dto.PostDto;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 import com.springboot.blog.ultis.AppConstant;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class PostController {
 
     //Create Blog post
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
@@ -44,7 +45,7 @@ public class PostController {
 
     //Update post by id
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePost( @RequestBody PostDto postDto, @PathVariable(name = "id") Long id) {
+    public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") Long id) {
 
         PostDto postResponse = postService.updatePost(postDto, id);
 
--- src/main/java/com/springboot/blog/payload/Dto/PostDto.java
+++ src/main/java/com/springboot/blog/payload/Dto/PostDto.java
@@ -1,5 +1,7 @@
 package com.springboot.blog.payload.Dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
@@ -7,9 +9,18 @@ import java.util.Set;
 @Data
 public class PostDto {
     private Long id ;
+
+    @NotEmpty
+    @Size(min = 2, message = "Post tittle should have at least 2 characters")
     private String title ;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description cannot be blank ")
     private String description ;
+
     private String author ;
+
+    @NotEmpty
     private String content ;
 
     private Set<CommentDto> comments ;
